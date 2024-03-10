6ad9ad79e624bc6941eebb58ca580bd9bdc25dd0
kietttv <tvtkiet2002@gmail.com>
2024-03-11 01:56:28 +0700

    Create uploadFile fuction and AddNewArticle action (not check bug)

---
+++ project_comp1640_be/Controllers/ContributionsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient.Cypher;
+using project_comp1640_be.Data;
+using project_comp1640_be.Model;
+
+namespace project_comp1640_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContributionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContributionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private void uploadFile(IFormFile file)
+        {
+            var filePaths = new List<string>();
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Articles", file.FileName);
+            filePaths.Add(filePath);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+        }
+
+        [HttpPost("Add-New-Article")]
+        public async Task<IActionResult> AddNewArticle([FromForm] Contributions contribution, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is not provided or empty.");
+            }
+
+            uploadFile(file);
+
+            return NoContent();
+        }
+    }
+}
